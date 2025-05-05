5bb834470acd97a149a35dc942326e340a48e282
caozhangni <474902474@qq.com>
2025-05-05 12:07:53 +0800

    czn

--- pkg/modules/dependencies.go
+++ pkg/modules/dependencies.go
@@ -5,12 +5,14 @@ const (
 	All string = "all"
 
 	Core                  string = "core"
+	// NOTE: 注意这个模块实际并没有被注册,所以配置这个模块在启动的时候会报找不到的错误
 	GrafanaAPIServer      string = "grafana-apiserver"
 	StorageServer         string = "storage-server"
 	ZanzanaServer         string = "zanzana-server"
 	InstrumentationServer string = "instrumentation-server"
 )
 
+// INFO: 定义模块之间的依赖关系
 var dependencyMap = map[string][]string{
 	GrafanaAPIServer: {InstrumentationServer},
 	StorageServer:    {InstrumentationServer},
